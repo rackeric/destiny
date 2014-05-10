4b51acf7c66e7fd786b6f0ed87216ea3060abaeb
utcodemonkey@hotmail.com <eric@destiny01-test.bashtothefuture.com>
2014-05-10 05:46:14 +0000

    removing test code from destingCelery

--- destinyCelery.py
+++ destinyCelery.py
@@ -1,10 +1,9 @@
-from ansible import utils
 from firebase import firebase, FirebaseApplication, FirebaseAuthentication
-from celery import Celery
-from celery import task
-import ansible.runner, json, os
+from celery import Celery, task
 from django.http import HttpResponse
 from firebase.jsonutil import JSONEncoder
+import ansible.runner, ansible.utils
+import json, os
 
 celery = Celery('destinyCelery', broker='amqp://guest@localhost//')
 
@@ -18,14 +17,13 @@ def ansible_jeneric(job_id, user_id):
     # set the specific job from firebase with user
     user = 'simplelogin:' + user_id
     URL = 'https://deploynebula.firebaseio.com/users/' + user + '/external_data/'
-    #myExternalData = Firebase(URL)
     #myExternalData = FirebaseApplication(URL)
     myExternalData = FirebaseApplication(URL, authentication)
 
     # update status to RUNNING in firebase
     myExternalData.patch(job_id, json.loads('{"status":"RUNNING"}'))
     
-    # finally, get the actual job
+    # finally, get the actual job and set ansible options
     job = myExternalData.get(URL, job_id)
 
     myHostList = job['host_list']
@@ -35,8 +33,10 @@ def ansible_jeneric(job_id, user_id):
     myRemoteUser = job['remote_user']
     myRemotePass = job['remote_pass']
 
+    # creating Ansible Inventory based on host_list
     myInventory = ansible.inventory.Inventory(myHostList)
  
+    # run ansible module
     results = ansible.runner.Runner(
         pattern=myPattern,
         forks=10,
@@ -47,34 +47,18 @@ def ansible_jeneric(job_id, user_id):
         inventory=myInventory,
     ).run()
     
-    # get it to a good format
-    #data = json.loads(results)
-    #data = json.dumps(results)
-
     # set status to COMPLETE
     myExternalData.patch(job_id, json.loads('{"status":"COMPLETE"}'))
 
-    #json_results = ansible.utils.jsonify(results)
-    #loads_results = json.loads(json_results)
-    #loads_results = json.dumps(results)
-    #print loads_results
+    # jsonify the results
     json_results = ansible.utils.jsonify(results)
 
-    #myJson = firebase.JSONEncoder(results)
-    #new_results = myJson.encode()
-    
-    # post results to firebase
-    #myExternalData.post(job_id + '/returns', unescape(json.loads(json_results)))
-    #myExternalData.post(job_id + '/returns/', results.decode('string-escape'))
-    #data = json.dumps(results, cls=JSONEncoder)
-
-    myExternalData.post(job_id + '/returns', json.loads(json_results))
-    #myExternalData.post(job_id + '/returns', firebase.json.loads(results))
-    #myExternalData.put(job_id + '/returns', json.loads('{"contacted": {"162.242.225.175": {"changed": false, "invocation": {"module_args": "", "module_name": "ping"}, "ping": "pong"}}, "dark": {}}'), connection=None)
-
+    #
+    # HELP! can't get a proper json object to pass, but below string works
+    #
+    myExternalData.post(job_id + '/returns', json_results)
     #myExternalData.patch(job_id + '/returns', json.loads('{"contacted": {"Cloud-Server-13": {"changed": true, "cmd": ["hostname"], "delta": "0:00:00.002057", "end": "2014-05-08 04:01:49.692771", "invocation": {"module_args": "hostname", "module_name": "command"}, "rc": 0, "start": "2014-05-08 04:01:49.690714", "stderr": "", "stdout": "cloud-server-13"}, "Cloud-Server-14": {"changed": true, "cmd": ["hostname"], "delta": "0:00:00.001994", "end": "2014-05-08 04:01:49.449724", "invocation": {"module_args": "hostname", "module_name": "command"}, "rc": 0, "start": "2014-05-08 04:01:49.447730", "stderr": "", "stdout": "cloud-server-14"}}, "dark": {}}'))
 
-    #returns.patch(job_id + '/returns', json.dumps(results))
     return json_results
 
 def ansible_jeneric_view(request, job_id, user_id):
